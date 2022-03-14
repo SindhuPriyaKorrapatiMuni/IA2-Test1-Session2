d61cd83e39736a03600aca59acc34b7d40d1c868
Sindhu Priya Korrapati <94669334+SindhuPriyaKorrapatiMuni@users.noreply.github.com>
2022-03-14 07:06:43 +0000

    done

--- p2final.c
+++ p2final.c
@@ -1,2 +1,38 @@
 #include<stdio.h>
-int
+int input_side()
+{
+  int a;
+  printf("Enter the side of the triangle\n");
+  scanf("%d",&a);
+  return a;
+}
+int check_scalene(int a,int b,int c)
+{
+  int count=0;
+  if(a!=b&&b!=c&&c!=a)
+  {
+    count++;
+  }
+  return count;
+}
+void output(int a,int b,int c,int isscalene)
+{
+  if(isscalene==1)
+  {
+    printf("The triangle with the sides %d,%d and %d is Scalene",a,b,c);
+  }
+  else
+  {
+    printf("The triangle with the sides %d,%d and %d is not scalene",a,b,c);
+  }  
+}
+int main()
+{
+  int a1,b1,c1,count1;
+  a1=input_side();
+  b1=input_side();
+  c1=input_side();
+  count1=check_scalene(a1,b1,c1);
+  output(a1,b1,c1,count1);
+  return 0;
+}
